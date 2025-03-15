cc421e3f9df94c6c26f4699f2dea664f748521a0
Aleksandr Petrov <petrov.aleksr@gmail.com>
2025-03-15 12:13:04 +0500

    feat: create basic routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,19 @@
 import AppLayout from './components/app-layout/app-layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+import About from './pages/about/about';
+import NotFound from './pages/not-found/not-found';
 
 const App = () => {
-  return <AppLayout />;
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
