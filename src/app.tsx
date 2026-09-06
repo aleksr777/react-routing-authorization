2b8b05546655e19ca2375df6c19459e5b10f6f38
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-06 23:25:02 +0500

    Add account settings routes

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,9 @@ import Login from './pages/login/login';
 import PasswordReset from './pages/password-reset/password-reset';
 import Registration from './pages/registration/registration';
 import MyProfile from './pages/my-profile/my-profile';
+import Settings from './pages/account-settings/settings';
+import ChangePassword from './pages/account-settings/change-password';
+import ChangeEmail from './pages/account-settings/change-email';
 import NotFound from './pages/not-found/not-found';
 
 const App = () => {
@@ -18,6 +21,9 @@ const App = () => {
         <Route path="auth/password-reset" element={<PasswordReset />} />
         <Route element={<ProtectedRoute />}>
           <Route path="users/me" element={<MyProfile />} />
+          <Route path="users/me/settings" element={<Settings />} />
+          <Route path="users/me/settings/password" element={<ChangePassword />} />
+          <Route path="users/me/settings/email" element={<ChangeEmail />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Route>
