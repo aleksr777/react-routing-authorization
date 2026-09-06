a05b27435ade8e8c80de4863d2ecbc28566ec9cd
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-06 23:04:40 +0500

    Disable current navigation link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,40 +1,47 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../../features/auth/model/use-auth';
 import styles from './header.module.css';
 
+type NavigationItemProps = {
+  to: string;
+  label: string;
+  currentPath: string;
+};
+
+const NavigationItem = ({ to, label, currentPath }: NavigationItemProps) => (
+  <li className={styles.li}>
+    {currentPath === to ? (
+      <span className={styles.currentLink}>{label}</span>
+    ) : (
+      <Link className={styles.link} to={to}>
+        <span className={styles.linkText}>{label}</span>
+      </Link>
+    )}
+  </li>
+);
+
 const Header = () => {
   const { isAuth, isInitializing } = useAuth();
+  const { pathname } = useLocation();
 
   return (
     <nav className={styles.nav}>
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <Link className={styles.link} to="/">
-            <span className={styles.linkText}>Home</span>
-          </Link>
-        </li>
+        <NavigationItem to="/" label="Home" currentPath={pathname} />
 
         {!isInitializing && !isAuth && (
           <>
-            <li className={styles.li}>
-              <Link className={styles.link} to="/auth/login">
-                <span className={styles.linkText}>Login</span>
-              </Link>
-            </li>
-            <li className={styles.li}>
-              <Link className={styles.link} to="/auth/registration">
-                <span className={styles.linkText}>Registration</span>
-              </Link>
-            </li>
+            <NavigationItem to="/auth/login" label="Login" currentPath={pathname} />
+            <NavigationItem
+              to="/auth/registration"
+              label="Registration"
+              currentPath={pathname}
+            />
           </>
         )}
 
         {!isInitializing && isAuth && (
-          <li className={styles.li}>
-            <Link className={styles.link} to="/users/me">
-              <span className={styles.linkText}>My profile</span>
-            </Link>
-          </li>
+          <NavigationItem to="/users/me" label="My profile" currentPath={pathname} />
         )}
       </ul>
     </nav>
