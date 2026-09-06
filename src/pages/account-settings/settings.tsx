d1a538b3b4ef98e43afd624e0c461a1135c263ae
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-06 23:24:25 +0500

    Add account settings page

---
+++ src/pages/account-settings/settings.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import styles from './account-settings.module.css';
+
+const Settings = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h1 className={styles.title}>Settings</h1>
+
+      <div className={styles.links}>
+        <Link className={styles.link} to="/users/me/settings/password">
+          Change password
+        </Link>
+        <Link className={styles.link} to="/users/me/settings/email">
+          Change email
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Settings;
