1f2fcec823b42edf8e2853cfaed5f57896cc466a
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-07 16:16:18 +0500

    Add profile deletion route

--- src/app.tsx
+++ src/app.tsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/my-profile/my-profile';
 import Settings from './pages/account-settings/settings';
 import ChangePassword from './pages/account-settings/change-password';
 import ChangeEmail from './pages/account-settings/change-email';
+import DeleteProfile from './pages/account-settings/delete-profile';
 import NotFound from './pages/not-found/not-found';
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
           <Route path="users/me/settings" element={<Settings />} />
           <Route path="users/me/settings/password" element={<ChangePassword />} />
           <Route path="users/me/settings/email" element={<ChangeEmail />} />
+          <Route path="users/me/settings/delete" element={<DeleteProfile />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Route>
