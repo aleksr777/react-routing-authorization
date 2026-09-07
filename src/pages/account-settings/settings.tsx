27563c7bcd6d2a5f2872a239bb8ea9e22ead7c40
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-07 16:16:08 +0500

    Add profile deletion link to settings

--- src/pages/account-settings/settings.tsx
+++ src/pages/account-settings/settings.tsx
@@ -13,6 +13,9 @@ const Settings = () => {
         <Link className={styles.link} to="/users/me/settings/email">
           Change email
         </Link>
+        <Link className={styles.dangerLink} to="/users/me/settings/delete">
+          Delete profile
+        </Link>
         <Link className={styles.link} to="/users/me">
           Back to profile
         </Link>
